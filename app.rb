ce8b5456bbb6f6f1ac781ec785da9b5f93189ed3
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-18 06:54:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
   get '/' do
     erb :user_input
+  end
+
+  post 'piglatinize' do 
+    @piglatinizer 
   end
 end
